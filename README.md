c4f7811c94ae74457b1397aa48f0a6b46b7415e8
Tonieie <tonzazil12@gmail.com>
2021-02-26 18:23:58 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Rotating_7segments
+ AVR Assembly to ratates 0-9 on 7-segments
---
+++ main.asm
@@ -0,0 +1,12 @@
+;
+; Rotating_7segments.asm
+;
+; Created: 26-Feb-21 6:22:22 PM
+; Author : Tonie
+;
+
+
+; Replace with your application code
+start:
+    inc r16
+    rjmp start
