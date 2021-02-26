71b709e78103657a0eca0277fd164504d747288b
Tonieie <tonzazil12@gmail.com>
2021-02-26 18:41:35 +0700

    change .equ constant to capital letter

--- main.asm
+++ main.asm
@@ -6,27 +6,29 @@
 ;
 ; Replace with your application code
 
-.equ digitDDR	= DDRD
-.equ digitPort	= PORTD
-.equ digit_1	= 5
-.equ digit_2	= 4
-.equ digit_3	= 3
-.equ digit_4	= 2
+.equ DIGITDDR	= DDRD
+.equ DIGITPORT	= PORTD
+.equ DIGIT1	= 5
+.equ DIGIT2	= 4
+.equ DIGIT3	= 3
+.equ DIGIT4	= 2
 
-.equ BcdDDR		= DDRB
-.equ BcdPort	= PORTB
-.equ Bcd_A		= 0
-.equ Bcd_B		= 1
-.equ Bcd_C		= 2
-.equ Bcd_D		= 3
+.equ BCDDDR		= DDRB
+.equ BCDPORT	= PORTB
+.equ BCDA		= 0
+.equ BCDB		= 1
+.equ BCDC		= 2
+.equ BCDD		= 3
 
-.equ SwDDR		= DDRC
-.equ SwPort		= PORTC
-.equ SwPin		= PINC
-.equ Sw_ror		= 3
-.equ Sw_rol		= 2
-.equ Sw_pause		= 1
-.equ Sw_reset		= 0
+.equ SWDDR		= DDRC
+.equ SWPORT		= PORTC
+.equ SWPIN		= PINC
+.equ SWROR		= 3
+.equ SWROL		= 2
+.equ SWPAUSE		= 1
+.equ SWRESET		= 0
+
+.def head = r18
 
 .org 0x00
 setup :
@@ -36,13 +38,13 @@ setup :
 	out SPL,r16
 
     ldi r16,0xFF
-	out BcdDDR,r16
-	out digitDDR,r16
+	out BCDDDR,r16
+	out DIGITDDR,r16
 
 	ldi r16,0x00
-	out SwDDR,r16
+	out SWDDR,r16
 	ldi r16,0xFF
-	out SwPort,r16
+	out SWPORT,r16
 
 delay_5ms :	ldi r20,80
 	outer_loop : ldi r21,250	
