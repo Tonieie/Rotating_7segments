07a9b4a55509c4302e17dba0d9fdf7d59881bcf2
Tonieie <tonzazil12@gmail.com>
2021-02-27 21:14:13 +0700

    changed check_toggle to read_input and added debounce

--- main.asm
+++ main.asm
@@ -28,9 +28,9 @@
 .equ SWPAUSE		= 1
 .equ SWRESET		= 0
 
-.equ last_input_addr	= 0x0100
-.equ mode_addr			= 0x0101
-.equ last_mode_addr		= 0x0102
+.equ last_input	= 0x0100
+.equ mode			= 0x0101
+.equ is_pause			= 0x0102
 
 .def head = r18
 .def loop_counter = r17
@@ -55,8 +55,11 @@ setup :
 	ldi r16,0xFF
 	out SWPORT,r16
 
+	ldi r16,0x00
+	sts is_pause,r16
+
 	ldi r16,0x0F
-	sts last_input_addr,r16			//assume switches haven't presssed
+	sts last_input,r16			//assume switches haven't presssed
 
 	ldi ZH,HIGH(my_num << 1)
 
@@ -64,44 +67,72 @@ reset :
 	ldi head,10					//set head start from 0
 	ldi loop_counter,25			// 1 loop = 5ms * 4 = 20ms, but we want to rotate every 1s so set loop_counter to 50 times = 20ms * 50 = 1s
 halt:
+	call read_input
 	call delay_5ms
-	call check_toggle
-	brcs jump_to_loop
-	rjmp halt
+	call delay_5ms
+	call debounce
+	brcs halt
+	rjmp rotate_loop
 
-jump_to_loop :
-	lds r20,mode_addr
+read_input :
+	in current_input,SWPIN
+	andi current_input,0x0F
+	lds r16,last_input
+	cpse r16,current_input
+	sts last_input,current_input
+	ret
 
-	sbrs r20,SWROR
-	rjmp ror_loop
-	sbrs r20,SWROL
-	rjmp rol_loop
-
-	rjmp halt
-
-check_toggle :
+debounce :
 	in current_input,SWPIN
 	andi current_input,0x0F
 
-	lds r20,last_input_addr
-	cp current_input,r20
-	brne set_mode
-	sts last_input_addr,current_input
+	lds r16,last_input
+	cp current_input,r16
+	breq set_mode
 	clc
 	ret
-
 	set_mode :
-		cpi current_input,0x0F
-		brne store_mode
+		cpi current_input,0b00001110
+		breq set_reset_mode
+		cpi current_input,0b00000111
+		breq set_ror_mode
+		cpi current_input,0b00001011
+		breq set_rol_mode
+		cpi current_input,0b00001101
+		breq set_pause_mode
+		
+		clc
 		ret
-		store_mode :
-			lds r20,mode_addr
-			sts last_mode_addr,r20		//store current_mode to last_mode
-			sts mode_addr,current_input	//store new current_mode to current_mode
-			sec
-			ret	
-
-
+	set_reset_mode :
+		ldi r16,0
+		sts mode,r16
+		sec
+		ret
+	set_ror_mode :
+		ldi r16,1
+		sts mode,r16
+		sec
+		ret
+	set_rol_mode :
+		ldi r16,2
+		sts mode,r16
+		sec
+		ret
+	set_pause_mode :
+		lds r16,is_pause
+		cpi r16,0
+		breq set_pause
+		cpi r16,1
+		breq clr_pause
+		ret
+		set_pause :
+			ser r16
+			sts is_pause,r16
+			ret
+		clr_pause :
+			clr r16
+			sts is_pause,r16
+			ret
 
 rol_loop :	
 		ldi loop_counter,25		//set it to 50 again
@@ -142,7 +173,7 @@ rotate_loop :
 	
 	mov ZL,head			//copy head to r16
 	
-	call check_toggle
+	call read_input
 
 	sbi DIGITPORT,DIGIT1
 	call next_num
@@ -159,6 +190,8 @@ rotate_loop :
 	call delay_5ms			//toggle each digit by 5ms
 	cbi DIGITPORT,DIGIT2
 
+	call debounce
+	call read_input
 
 	sbi DIGITPORT,DIGIT3
 	call next_num
@@ -174,18 +207,32 @@ rotate_loop :
 	call delay_5ms			//toggle each digit by 5ms
 	cbi DIGITPORT,DIGIT4
 
+	call debounce
+
+	lds r16,is_pause
+	cpi r16,0xFF
+	breq rotate_loop
+	cpi r16,0
+	breq dec_counter
 	
-	dec loop_counter		//decrease the loop counter
-	brne rotate_loop			//if loop_counter != 0 loop again
+	dec_counter :
+		dec loop_counter		//decrease the loop counter
+		brne rotate_loop			//if loop_counter != 0 loop again
 
-	lds r20,mode_addr
-	sbrs r20,SWROR
-	rjmp ror_loop
-	sbrs r20,SWROL
-	rjmp rol_loop
-	sbrs r20,SWRESET
-	rjmp reset
-
+	lds r16,mode
+	cpi r16,0
+	breq b4_reset
+	cpi r16,1
+	breq b4_ror
+	cpi r16,2
+	breq b4_rol
+	rjmp rotate_loop
+	b4_reset :
+		rjmp reset
+	b4_ror :
+		rjmp ror_loop
+	b4_rol :
+		rjmp rol_loop
       
 delay_5ms :	ldi r20,80
 	outer_loop : ldi r21,250	
